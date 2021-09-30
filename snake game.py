cba4ea029ea555a31467bc6fa94c390772fe7b39
Piyush <70484570+piyush7900@users.noreply.github.com>
2021-09-30 23:57:59 +0530

    Update snake game.py

--- snake game.py
+++ snake game.py
@@ -4,6 +4,7 @@ import random
  
 pygame.init()
  
+ 
 white = (255, 255, 255)
 yellow = (255, 255, 102)
 black = (0, 0, 0)
@@ -13,6 +14,7 @@ blue = (50, 153, 213)
  
 dis_width = 600
 dis_height = 400
+ 
  
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption('Snake Game by Edureka')
@@ -24,6 +26,7 @@ snake_speed = 15
  
 font_style = pygame.font.SysFont("bahnschrift", 25)
 score_font = pygame.font.SysFont("comicsansms", 35)
+ 
  
  
 def Your_score(score):
